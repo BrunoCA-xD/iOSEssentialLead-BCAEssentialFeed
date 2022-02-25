771b32a19b45616aec9a42a2216a2d020ca0d6b9
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-02-25 11:13:15 -0300

    Add memory leak tracking

--- BCAEssentialFeedAPIEndToEndTests/BCAEssentialFeedAPIEndToEndTests.swift
+++ BCAEssentialFeedAPIEndToEndTests/BCAEssentialFeedAPIEndToEndTests.swift
@@ -20,11 +20,12 @@ final class BCAEssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helper func
     
-    private func getFeedResult() -> RemoteFeedLoader.Result?{
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> RemoteFeedLoader.Result?{
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "wait for load completion")
         
         var receivedResult: RemoteFeedLoader.Result?
