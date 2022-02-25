f29a425a1074fa95cc59636db3263de2831f6aaf
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-02-25 11:09:18 -0300

    Add end-to-end API tests in a separated test target

---
+++ BCAEssentialFeedAPIEndToEndTests/BCAEssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,110 @@
+import XCTest
+
+@testable import BCAEssentialFeed
+
+final class BCAEssentialFeedAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "wait for load completion")
+        
+        var receivedResult: RemoteFeedLoader.Result?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 15)
+        
+        switch receivedResult {
+        case let .success(items)?:
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+            items.enumerated().forEach { (index, item) in
+                XCTAssertItemsEquals(item, expectedItem(at: index), comparedIndex: index)
+            }
+        case let .failure(error)?:
+            XCTFail("Expected success, got \(error) instead")
+        default:
+            XCTFail("Expected success, got nil instead")
+        }
+    }
+    
+    // MARK: - Helper func
+    
+    private func XCTAssertItemsEquals(_ item1: FeedItem?, _ item2: FeedItem?, comparedIndex: Int, file: StaticString = #filePath, line: UInt = #line) {
+        let failMessageSuffix = "does not match at index: \(comparedIndex)"
+        
+        if item1?.id != item2?.id {
+            XCTFail("Items id \(failMessageSuffix)", file: file, line: line)
+        } else if item1?.description != item2?.description {
+            XCTFail("Items description \(failMessageSuffix)", file: file, line: line)
+        } else if item1?.location != item2?.location {
+            XCTFail("Items location \(failMessageSuffix)", file: file, line: line)
+        } else if item1?.imageURL != item2?.imageURL {
+            XCTFail("Items imageURL \(failMessageSuffix)", file: file, line: line)
+        }
+    }
+    
+    private func expectedItem(at index: Int) -> FeedItem? {
+        FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index)
+        )
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: [
+            "https://url-1.com",
+            "https://url-2.com",
+            "https://url-3.com",
+            "https://url-4.com",
+            "https://url-5.com",
+            "https://url-6.com",
+            "https://url-7.com",
+            "https://url-8.com"
+        ][index])!
+    }
+}
