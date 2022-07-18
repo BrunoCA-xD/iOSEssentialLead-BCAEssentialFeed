a6e0645aa9f410c6e569d4c36d1d60ae7306a8b5
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-07-18 20:47:28 -0300

    Remove force unwrap from production code

--- BCAEssentialFeed/Sources/FeedCache/infrastructure/CoreData/ManagedCache.swift
+++ BCAEssentialFeed/Sources/FeedCache/infrastructure/CoreData/ManagedCache.swift
@@ -13,7 +13,9 @@ extension ManagedCache {
     }
     
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
-        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        guard let entityName = ManagedCache.entity().name
+        else { return nil }
+        let request = NSFetchRequest<ManagedCache>(entityName: entityName)
         request.returnsObjectsAsFaults = false
         
         return try context.fetch(request).first
