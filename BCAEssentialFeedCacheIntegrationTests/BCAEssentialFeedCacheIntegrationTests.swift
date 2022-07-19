0eebc1398eb52503cee96aa068b4cb753b378c4c
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-07-19 07:34:37 -0300

    Clean up & undo all cache side-effects on `setUp` and `tearDown` to avoid sharing state between tests

--- BCAEssentialFeedCacheIntegrationTests/BCAEssentialFeedCacheIntegrationTests.swift
+++ BCAEssentialFeedCacheIntegrationTests/BCAEssentialFeedCacheIntegrationTests.swift
@@ -1,7 +1,20 @@
 import XCTest
 import BCAEssentialFeed
+@testable import BCAEssentialFeedTests
 
 class BCAEssentialFeedCacheIntegrationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
 
     func test_load_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
@@ -38,5 +51,17 @@ class BCAEssentialFeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
