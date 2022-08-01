1473849a00f5e8bd81dc5165d39bb59d2a03f477
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-08-01 17:03:03 -0300

    Move `FeedViewController` to production target

--- BCAEssentialFeediOSTests/FeedViewControllerTests.swift
+++ BCAEssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,33 +1,7 @@
 import XCTest
 import UIKit
 import BCAEssentialFeed
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        
-        load()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import BCAEssentialFeediOS
 
 final class FeedViewControllerTests: XCTestCase {
     
@@ -97,7 +71,6 @@ private extension FeedViewController {
         refreshControl?.isRefreshing == true
     }
 }
-
 
 private extension UIRefreshControl {
     func simulatePullToRefresh() {
