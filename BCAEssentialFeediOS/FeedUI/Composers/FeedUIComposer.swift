3d8f5e6d5d1539e8c58f36b3ddd5ca7315b36d1b
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-08-26 10:16:45 -0300

    Decouple `FeedPresenter` from `FeedLoader` with an adapter in the Composition layer.

--- BCAEssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ BCAEssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -5,8 +5,9 @@ public final class FeedUIComposer {
     private init() { }
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let feedPresenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshViewController(loadFeed: feedPresenter.loadFeed)
+        let feedPresenter = FeedPresenter()
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: feedPresenter)
+        let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
         let feedController = FeedViewController(refreshController: refreshController)
         feedPresenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         feedPresenter.loadingView = WeakRefVirtualProxy(object: refreshController)
@@ -50,5 +51,29 @@ private final class FeedViewAdapter: FeedView {
         controller?.tableModel = viewModel.feed.map({ model in
             FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
         })
+    }
+}
+
+
+private final class FeedLoaderPresentationAdapter {
+    private let feedLoader: FeedLoader
+    private let presenter: FeedPresenter
+    
+    init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+        self.feedLoader = feedLoader
+        self.presenter = presenter
+    }
+    
+    func loadFeed() {
+        presenter.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case .success(let feed):
+                self?.presenter.didFinishLoadingFeed(with: feed)
+            case .failure(let error):
+                self?.presenter.didFinishLoadingFeed(with: error)
+            }
+        }
     }
 }
