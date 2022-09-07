de4f1cd53db3c87da74afc42e1008dcee35b6078
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-09-07 19:06:54 -0300

    Move FeedLoaderPresentationAdapter to its own file

---
+++ BCAEssentialFeediOS/FeedUI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,24 @@
+import Foundation
+import BCAEssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case .success(let feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case .failure(let error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
