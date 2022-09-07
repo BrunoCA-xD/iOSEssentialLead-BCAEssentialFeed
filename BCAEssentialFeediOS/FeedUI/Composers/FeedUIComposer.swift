ac310e07221acbfedea6ea7c8589fa3668b9de28
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-09-07 18:54:44 -0300

    Dispatch background feed image data completion to main thread before passing result to the UI components. Threading is dealt with a Decorator in the Composition layer

--- BCAEssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ BCAEssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -12,7 +12,9 @@ public final class FeedUIComposer {
             title: FeedPresenter.title
             )
         presentationAdapter.presenter = FeedPresenter(
-            feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+            feedView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
             loadingView: WeakRefVirtualProxy(object: refreshController))
         
         return feedController
@@ -33,9 +35,20 @@ private final class MainQueueDispatchDecorator<T> {
         completion()
     }
 }
+
 extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
             self?.dispatch {
                 completion(result)
             }
--- BCAEssentialFeediOSTests/FeedUIIntegrationTests.swift
+++ BCAEssentialFeediOSTests/FeedUIIntegrationTests.swift
@@ -246,6 +246,21 @@ final class FeedUIIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    
+    func test_loadImageDataCompletion_dispatchesFromBrackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [makeImage()], at: 0)
+        _ = sut.simulteFeedImageViewVisible(at: 0)
+        
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeImageLoading(with: self.anyImageData(), at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     // MAKR: - Helpers
     
     func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
