5b5e3d579e5979e52fac58473e82478361dacef1
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-09-21 10:09:14 -0300

    Extract SUT creation to a factory helper method

--- BCAEssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ BCAEssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -9,14 +9,21 @@ final class FeedImagePresenter {
 final class FeedImagePresenterTests: XCTestCase {
     
    func test_init_doesNotSendMessagesToView() {
-       let view = ViewSpy()
+       let (_, view) = makeSUT()
        
-       _ = FeedImagePresenter(view: view)
-
        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
    }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        
+        return (sut, view)
+    }
     
     private class ViewSpy {
         var messages = [Any]()
