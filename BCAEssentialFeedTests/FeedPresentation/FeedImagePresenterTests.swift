a8f6027d086ac9fcf5e8a4df64206815e97f78b9
bruno.ambrosio <bruno.car.ambrosio@gmail.com>
2022-09-21 10:31:09 -0300

    `FeedImagePresenter` display loading and shouldRetry state on `didStartLoadingImageData`

--- BCAEssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ BCAEssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -1,8 +1,38 @@
 import XCTest
+import BCAEssentialFeed
+
+struct FeedImageViewModel: Equatable {
+    let description: String?
+    let location: String?
+    let image: Data?
+    let isLoading: Bool
+    let shouldRetry: Bool
+    
+    var hasLocation: Bool {
+        return location != nil
+    }
+}
+
+protocol FeedImageView {
+    
+    func display(_ model: FeedImageViewModel)
+}
 
 final class FeedImagePresenter {
-    init(view: Any) {
-        
+    private let view: FeedImageView
+    
+    init(view: FeedImageView) {
+        self.view = view
+    }
+    
+    func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false)
+        )
     }
 }
 
@@ -13,6 +43,18 @@ final class FeedImagePresenterTests: XCTestCase {
        
        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
    }
+    
+    func test_didStartLoadingImageData_shouldDisplaysLoadingStateAndShouldRetryState() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingImageData(for: uniqueImage())
+        
+        XCTAssertEqual(view.messages, [
+            .display(isLoading: true),
+            .display(shouldRetry: false)
+        ])
+        
+    }
     
     // MARK: - Helpers
     
@@ -25,7 +67,18 @@ final class FeedImagePresenterTests: XCTestCase {
         return (sut, view)
     }
     
-    private class ViewSpy {
-        var messages = [Any]()
+    private class ViewSpy: FeedImageView {
+        enum Message: Hashable {
+            case display(isLoading: Bool)
+            case display(shouldRetry: Bool)
+        }
+        
+        var messages = Set<Message>()
+        
+        func display(_ model: FeedImageViewModel) {
+            messages.insert(.display(isLoading: model.isLoading))
+            messages.insert(.display(shouldRetry: model.shouldRetry))
+        }
     }
+    
 }
